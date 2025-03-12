ffbc7a6b26ef2cce1120d8f66c3e08be10481acf
Oleksandr <sigmanor@pm.me>
2025-03-12 10:08:15 +0000

    add screenshot.js

---
+++ .github/scripts/screenshot.js
@@ -0,0 +1,22 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const path = require("path");
+
+(async () => {
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
+  const page = await browser.newPage();
+
+  // Відкриваємо сторінку
+  const url = "https://profile-summary-for-github.com/user/Sigmanor";
+  await page.goto(url, { waitUntil: "networkidle2" });
+
+  // Робимо скріншот
+  const filePath = path.join(__dirname, "../../assets/profile-summary.png");
+  await page.screenshot({ path: filePath, fullPage: true });
+
+  await browser.close();
+
+  console.log("✅ Screenshot saved:", filePath);
+})();
